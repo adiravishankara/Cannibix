11ca07a0b5d6735568a14e8b2b331afe2384c2e3
adiravishankara <adiravishankara@gmail.com>
2019-05-02 10:26:45 -0700

    Included all April Tests

--- test_call.py
+++ test_call.py
@@ -15,8 +15,10 @@ import Adafruit_ADS1x15
 from numpy import genfromtxt
 import math
 from combined_expose_collect2 import *
-from combined_expose_collect2 import full_run_test
-import combined_expose_collect1
+from combined_expose_collect2 import run_test
+#import combined_expose_collect1
+run_test()
+
 #full_run_test()
 #os.system('combined_expose_collect1.py')
 #combined_expose_collect()
